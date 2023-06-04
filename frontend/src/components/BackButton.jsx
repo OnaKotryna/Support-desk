461a5a61abcf2d5354bed6da804eadb207ab5eeb
Ona Kotryna <ok.kuncyte@gmail.com>
2023-06-05 00:41:12 +0300

    Add back button

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,12 @@
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import React from "react";
+
+const BackButton = ({ url }) => {
+  return (
+    <Link to={url} className="btn btn-reverse btn-back">
+      <FaArrowAltCircleLeft /> Back
+    </Link>
+  );
+};
+export default BackButton;
