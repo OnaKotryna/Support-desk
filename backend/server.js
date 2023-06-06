3c8b63921cfb36bcb7c638c3a14ca7c5e2e8fecc
Ona Kotryna <ok.kuncyte@gmail.com>
2023-06-06 16:21:52 +0300

    Fix message

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to database
 connectDB();
@@ -33,6 +33,4 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(errorHandler);
 
-app.listen(PORT, () =>
-  console.log(`Server started on port ${this.address().port}`)
-);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
